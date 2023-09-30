9a2821141a55bfce9914c1dfd2b12c0dc8fdad73
Aman Kumar <amankumar7506@gmail.com>
2023-09-29 21:43:24 -0400

    add post css

--- src/components/Main.js
+++ src/components/Main.js
@@ -31,6 +31,53 @@ const Main = (props) => {
           </button>
         </div>
       </Sharebox>
+      <div>
+        <Article>
+          <SharedActor>
+            <a>
+              <img src="/images/user.svg" alt="" />
+              <div>
+                <span> Title </span>
+                <span> Info </span>
+                <span> Date </span>
+              </div>
+            </a>
+            <button>
+              <img src="/images/ellipsis.svg" alt="" />
+            </button>
+          </SharedActor>
+          <Description> Description </Description>
+          <SharedImg>
+            <img src="/images/shared-img.png" alt="" />
+          </SharedImg>
+          <SocialCounts>
+            <li>
+              <button>
+                <img
+                  src="/images/like-icon.svg"
+                  alt="Like icon"
+                  style={{ height: "20px", width: "20px" }}
+                />
+
+                <img
+                  src="/images/appreciate.png"
+                  alt=""
+                  style={{ height: "20px", width: "20px" }}
+                />
+              </button>
+              <span> 75 </span>
+            </li>
+            <li>
+              <a> 2 comments </a>
+            </li>
+          </SocialCounts>
+          <button>
+            <img src="/images/like-icon.svg" alt="" />
+            <span> Like </span>
+          </button>
+          <button></button>
+        </Article>
+      </div>
     </Container>
   );
 };
@@ -73,8 +120,8 @@ const Sharebox = styled(CommonCard)`
 
     &:first-child {
       display: flex;
-      align-items: center; // I adjusted this to center align, since the previous value seemed incorrect.
-      padding: 8px 16px; // Use padding for spacing around items
+      align-items: center;
+      padding: 8px 16px;
 
       img {
         width: 48px;
@@ -111,6 +158,103 @@ const Sharebox = styled(CommonCard)`
         height: 20px;
         margin-right: 8px;
       }
+    }
+  }
+`;
+
+const Article = styled(CommonCard)`
+  padding: 0;
+  margin: 0 0px;
+  overflow: visible;
+`;
+
+const SharedActor = styled.div`
+  padding-right: 40px;
+  flex-wrap: nowrap;
+  padding: 12px 16px;
+  margin-bottom: 8px;
+  align-items: center;
+  display: flex;
+  a {
+    margin-right: 12px;
+    flex-grow: 1;
+    overflow: hidden;
+    display: flex;
+    text-decoration: none;
+
+    img {
+      width: 48px;
+      height: 48px;
+    }
+    & > div {
+      display: flex;
+      flex-direction: column;
+      flex-grow: 0;
+      margin-left: 8px;
+      overflow: hidden;
+      span {
+        text-align: left;
+        &:first-child {
+          font-size: 14px;
+          font-weight: 700;
+          color: rgba(0, 0, 0, 1);
+        }
+
+        &:nth-child(n + 1) {
+          font-size: 12px;
+          color: rgba(0, 0, 0, 0.6);
+        }
+      }
+    }
+  }
+
+  button {
+    position: absolute;
+    right: 12px;
+    top: 0;
+    background: transparent;
+    border: none;
+    outline: none;
+    img {
+      width: 30px;
+      height: 30px;
+    }
+  }
+`;
+
+const Description = styled.div`
+  padding: 0 16px;
+  overflow: hidden;
+  color: rgba(0, 0, 0, 0.9);
+  font-size: 14px;
+  text-align: left;
+`;
+const SharedImg = styled.div`
+  margin-top: 8px;
+  width: 100%;
+  display: block;
+  position: relative;
+  background-color: #f9fafb;
+  img {
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
+  }
+`;
+const SocialCounts = styled.ul`
+  line-height: 1.3;
+  display: flex;
+  align-items: flex-start;
+  overflow: auto;
+  margin: 0 16px;
+  padding: 8px 0;
+  border-bottom: 1px solid #e9e5df;
+  list-style: none;
+  li {
+    margin-right: 5px;
+    font-size: 12px;
+    button {
+      display: flex;
     }
   }
 `;
